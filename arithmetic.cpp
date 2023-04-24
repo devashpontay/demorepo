cd77ad7910e016b59728893b03e623d19572f9e2
Ezekiele2 <ezekielbillona@gmail.com>
2023-04-24 20:06:19 +0800

    Added subraction function.cpp

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -5,6 +5,10 @@ int add(int x, int y) {
     return x + y;
 }
 
+int sub(int x, int y){
+    return x - y;
+}
+
 
 
 int main () {
@@ -13,6 +17,11 @@ int main () {
     int x, y;
     std::cin >> x >> y;
     std::cout << "The answer is: " << add(x, y);
+
+    std::cout << "\n\nAdd two number for subraction:";
+    int sx, sy;
+    std::cin >> sx >> sy;
+    std::cout << "\nThe answer is: " << sub(sx, sy);
 
     return 0;
 }
